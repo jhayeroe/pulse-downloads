720a9e191b0b7a2fea4d1045d63360336dc7d962
jhayeroe <erdwinjayegaspar@gmail.com>
2026-09-02 06:43:20 +0800

    Add Drea proof-of-payment Messenger destination for v1.3.23

---
+++ rs8-pickleball-queue/updater/booking_config_v1323.php
@@ -0,0 +1,4 @@
+<?php
+// Public booking/payment destinations.
+if(!defined('RS8_DREA_MESSENGER_URL')) define('RS8_DREA_MESSENGER_URL','https://www.facebook.com/messages/e2ee/t/7347444785310560');
+function rs8DreaMessengerUrl(): string { return RS8_DREA_MESSENGER_URL; }
